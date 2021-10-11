8b20f15d00068461f72dd35ade05539ae649686c
totospl <totospl99@gmail.com>
2021-10-11 02:15:44 +0200

    init

---
+++ cpu_test/README.md
@@ -0,0 +1,2 @@
+# CPU Test version
+This is version written in Python3 using NumPy. The main goal is to develop mathematics for GPU version.
---
+++ cpu_test/StochNN.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+import numpy as np
+
+
+class StochNN:
+    def __init__(self, inputSize: int, hiddenLayers: List[int], outputSize: int):
+        if len(hiddenLayers) == 0:
+            raise ValueError("Netowork must have hidden layer!")
+
+        self.inputSize = inputSize
+        self.weights_list: List[np.mat] = []
+
+        inputWeights = np.zeros((hiddenLayers[0], inputSize))
+        self.weights_list.append(inputWeights)
+        for i, layerSize in enumerate(hiddenLayers):
+            if(i == 0):
+                continue
+            weights = np.zeros((layerSize, hiddenLayers[i - 1]))
+            self.weights_list.append(weights)
+        outputWeights = np.zeros((outputSize, hiddenLayers[-1]))
+        self.weights_list.append(outputWeights)
+        self.randomize_weights()
+
+    def randomize_weights(self, maxRand: float = 1.0):
+        self.weights_list[:] = [maxRand * np.random.rand(
+            *w.shape) for w in self.weights_list]
+
+    def feed_forward(self, input: np.array):
+        if(input.shape[0] != self.inputSize):
+            raise ValueError("Input Size error!")
+        
+        middleArr = np.array([])
+        for i, w in enumerate(self.weights_list):
+            if i == 0:
+                middleArr = np.matmul(w, input)
+            else:
+                middleArr = np.matmul(w, middleArr)
+            print(middleArr)
+        return middleArr
+
+        # def activation_func(self, ):
+
+
+if __name__ == "__main__":
+    nn = StochNN(2, [3, 4], 1)
+    nn.feed_forward(np.array([1, 1]))
