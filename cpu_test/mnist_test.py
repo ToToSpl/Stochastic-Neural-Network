9d1b6291b48ee7e99b5cc125f5e76d7307613f5c
totospl <totospl99@gmail.com>
2021-11-18 23:19:49 +0100

    mnist_test

---
+++ cpu_test/mnist_test.py
@@ -0,0 +1,34 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from StochNN import StochNN
+import numpy as np
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape((X_train.shape[0], num_pixels)).astype('float32')
+X_test = X_test.reshape((X_test.shape[0], num_pixels)).astype('float32')
+
+X_train = X_train / 255
+X_test = X_test / 255
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+model = StochNN(num_pixels, [3*num_pixels, 2*num_pixels], num_classes)
+model.load_model("mnist.stoch")
+
+accuracy = 0.0
+acc_matrix = np.zeros((num_classes, num_classes))
+
+for i, x in enumerate(X_test):
+    if i % 100 == 0:
+        print(i)
+    guess = np.argmax(model.feed_forward(x))
+    target = np.argmax(y_test[i])
+    acc_matrix[target, guess] += 1
+    if target == guess:
+        accuracy += 1
+
+print(acc_matrix)
+print(accuracy / y_test.shape[0])
