fec2bf7e2185dcb86714065bcec75dbfd20f2dfc
totospl <totospl99@gmail.com>
2021-11-18 23:25:21 +0100

    changed mnist training

--- cpu_test/stoch_mnist.py
+++ cpu_test/stoch_mnist.py
@@ -3,6 +3,8 @@ from tensorflow.keras.utils import to_categorical
 from StochNN import StochNN
 import numpy as np
 
+EPOCHS = 10
+
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 num_pixels = X_train.shape[1] * X_train.shape[2]
 X_train = X_train.reshape((X_train.shape[0], num_pixels)).astype('float32')
@@ -15,14 +17,16 @@ y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 num_classes = y_test.shape[1]
 
-model = StochNN(num_pixels, [3*num_pixels, 2*num_pixels], num_classes)
+model = StochNN(num_pixels, [3*num_pixels], num_classes)
 
-for i in range(4):
-    print(f"epoch: {i}")
-    for i, t in enumerate(X_train):
+for i in range(EPOCHS):
+    print("new epoch")
+    for j, t in enumerate(X_train):
         predictions = []
         res = model.feed_forward(t)
-        model.backpropagation(y_train[i], res)
-        print(i)
+        model.backpropagation(y_train[j], res)
+        if j % 100 == 0:
+            print(f"epoch: {i}\t{j/X_train.shape[0]}%...")
+    model.save_model(f"mnist_{i}")
 
-model.save_model("mnist.stoch")
+model.save_model("mnist_final.stoch")
