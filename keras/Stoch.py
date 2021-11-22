711b1b5eec096a114737dbf7938ecf660fd44536
Jacek Grzybowski <totospl99@gmail.com>
2021-11-22 02:51:58 +0100

    keras layer

---
+++ keras/Stoch.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+class Stochastic(keras.layers.Layer):
+    def __init__(self, units=32, input_dim=None):
+        super(Stochastic, self).__init__()
+        self.units = int(units)
+        self.input_dim = input_dim
+
+    def build(self, input_shape):
+        in_shape = self.input_dim if self.input_dim != None else input_shape[-1]
+        w_init = tf.random_uniform_initializer(minval=-0.1, maxval=0.1)
+        self.w = tf.Variable(
+            initial_value=w_init(
+                shape=(in_shape, self.units), dtype="float32"),
+            trainable=True,
+        )
+        b_init = tf.random_uniform_initializer(minval=-0.1, maxval=0.1)
+        self.b = tf.Variable(
+            initial_value=b_init(shape=(self.units,), dtype="float32"), trainable=True
+        )
+
+    def call(self, inputs):
+        sig_out = tf.math.sigmoid(tf.matmul(inputs, self.w) + self.b)
+        random_vals = tf.random.uniform(
+            shape=(self.units,), minval=0.0, maxval=1.0)
+        greater = tf.cast(tf.greater(sig_out, random_vals), dtype=tf.float32)
+        return 2.0 * greater - 1.0
---
+++ keras/test_mnist.py
@@ -0,0 +1,44 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+from Stoch import Stochastic
+
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, cohen_kappa_score
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape((X_train.shape[0], num_pixels)).astype('float32')
+X_test = X_test.reshape((X_test.shape[0], num_pixels)).astype('float32')
+
+X_train = X_train / 255
+X_test = X_test / 255
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+model = Sequential()
+model.add(Stochastic(2*num_pixels, input_dim=num_pixels))
+model.add(Stochastic(2*num_pixels))
+model.add(Dense(num_classes, activation='sigmoid'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd', metrics=['accuracy'])
+model.optimizer.lr.assign(0.00005)
+
+history = model.fit(X_train, y_train, validation_data=(
+    X_test, y_test), epochs=16, batch_size=1, verbose=1)
+
+testResults = model.predict(X_test)
+
+print(confusion_matrix(y_test.argmax(axis=1), testResults.argmax(axis=1)))
+print(classification_report(y_test.argmax(axis=1), testResults.argmax(axis=1)))
+print("Cohen's Kappa: {}".format(cohen_kappa_score(
+    y_test.argmax(axis=1), testResults.argmax(axis=1))))
+print("Accuracy: ", accuracy_score(
+    y_test.argmax(axis=1), testResults.argmax(axis=1)))
+
+model.save("keras_model.h5")
