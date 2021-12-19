f96c29f713bd75f30374b39a39b323ffdbbd8216
Jacek Grzybowski <totospl99@gmail.com>
2021-12-19 01:23:50 +0100

    better output

--- gpu/test_mnist.py
+++ gpu/test_mnist.py
@@ -1,8 +1,11 @@
+import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import cohen_kappa_score, confusion_matrix, ConfusionMatrixDisplay
 from StochNN_GPU import StochNN_GPU
 import time
 import cupy as cp
+import numpy as np
 
 _, (X_test, y_test) = mnist.load_data()
 num_pixels = X_test.shape[1] * X_test.shape[2]
@@ -22,21 +25,27 @@ for i, x in enumerate(X_test):
     y_test_gpu.append(cp.asarray(y_test[i], dtype=cp.float32))
 print(f"load time: {time.time() - begin}")
 
-model = StochNN_GPU(num_pixels, [3*num_pixels, 2*num_pixels], num_classes)
-model.load_model("mnist_15.stoch")
+model = StochNN_GPU(num_pixels, [2*num_pixels, 2*num_pixels], num_classes)
+model.load_model("test_mnists/mnist_81proc.stoch")
 
 accuracy = cp.zeros(1, dtype=cp.int32)
-# acc_matrix = np.zeros((num_classes, num_classes))
+y_result = []
 
 begin = time.time()
 for i, x in enumerate(X_test_gpu):
     pred = model.feed_forward(x)
+    y_result.append(pred.get())
     guess = cp.argmax(pred)
     target = cp.argmax(y_test_gpu[i])
-    # acc_matrix[target, guess] += 1
     if target == guess:
         accuracy += 1
 print(f"ff time: {time.time() - begin}")
 
-# print(acc_matrix)
-print(accuracy.get() / y_test.shape[0])
+y_result = np.array(y_result)
+
+print("kappa:", cohen_kappa_score(y_test.argmax(axis=1), y_result.argmax(axis=1)))
+print("acc:", accuracy.get() / y_test.shape[0])
+disp = ConfusionMatrixDisplay(
+    confusion_matrix(y_test.argmax(axis=1), y_result.argmax(axis=1)))
+disp.plot(colorbar=False)
+plt.show()
