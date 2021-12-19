d8bdf98cc0abd09359c6ebf2907350b53fc25217
totospl <totospl99@gmail.com>
2021-12-19 20:30:50 +0100

    normalized fields

--- scripts/field_generator.py
+++ scripts/field_generator.py
@@ -2,24 +2,23 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-FIELD_CENTER = np.array([0.0, 0.0])
-SIDE_HALF = 5
-STEP_SIZE = 0.5
+FIELD_CENTER = np.array([0.5, 0.5])
+STEP_SIZE = 0.05
 
 x_vecs = []
 y_vecs = []
-for i in np.arange(-SIDE_HALF, SIDE_HALF + STEP_SIZE, STEP_SIZE):
-  for j in np.arange(-SIDE_HALF, SIDE_HALF + STEP_SIZE, STEP_SIZE):
+for i in np.arange(0, 1 + STEP_SIZE, STEP_SIZE):
+  for j in np.arange(0, 1 + STEP_SIZE, STEP_SIZE):
     vec = np.array([j, i], dtype=np.float32)
     x_vecs.append(vec)
-    vec_dir = np.array([-i, j], dtype=np.float32)
+    vec_dir = np.array([-j + FIELD_CENTER[0], -i + FIELD_CENTER[1]], dtype=np.float32)
     vec_dir = vec_dir / np.linalg.norm(vec_dir)
     y_vecs.append(vec_dir)
 
 x_vecs = np.array(x_vecs)
 y_vecs = np.array(y_vecs)
 
-with open("follower_circle.pckl", 'wb') as outfile:
+with open("follower_center.pckl", 'wb') as outfile:
   pickle.dump([x_vecs, y_vecs], outfile, pickle.HIGHEST_PROTOCOL)
 
 x = []
