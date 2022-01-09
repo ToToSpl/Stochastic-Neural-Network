b95feaf2bc0b4f2904d0bf7835fd59c76c341d17
totospl <totospl99@gmail.com>
2022-01-09 15:55:28 +0100

    last changes

--- scripts/field_generator.py
+++ scripts/field_generator.py
@@ -17,7 +17,8 @@ def field_center(i, j):
 
 
 def field_circle(i, j):
-    vec_dir = np.array([-i + FIELD_CENTER[0], j - FIELD_CENTER[1]], dtype=np.float32)
+    vec_dir = np.array(
+        [-i + FIELD_CENTER[0], j - FIELD_CENTER[1]], dtype=np.float32)
     if np.linalg.norm(vec_dir) == 0:
         vec_dir = np.array([0.0, 0.0])
     else:
@@ -40,15 +41,5 @@ y_vecs = np.array(y_vecs)
 with open("follower_circle.pckl", 'wb') as outfile:
     pickle.dump([x_vecs, y_vecs], outfile, pickle.HIGHEST_PROTOCOL)
 
-x = []
-y = []
-u = []
-v = []
-for i in range(x_vecs.shape[0]):
-    x.append(x_vecs[i, 0])
-    y.append(x_vecs[i, 1])
-    u.append(y_vecs[i, 0])
-    v.append(y_vecs[i, 1])
-
-plt.quiver(x, y, u, v)
+plt.quiver(x_vecs[:, 0], x_vecs[:, 1], y_vecs[:, 0], y_vecs[:, 1])
 plt.show()
